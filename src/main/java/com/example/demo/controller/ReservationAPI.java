e3e2bcce073999525510b2ad4dbb8c4c5d71216f
Itai Gramse <ithai.g@gmail.com>
2021-01-19 14:32:19 +0100

    allow all origins

--- src/main/java/com/example/demo/controller/ReservationAPI.java
+++ src/main/java/com/example/demo/controller/ReservationAPI.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class ReservationAPI {
 
     private final ReservationService reservationService;
